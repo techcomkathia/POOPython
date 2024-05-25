4318a44bf0c58cb149440c0862de787e503b7c4a
Tech com Káthia <125410101+techcomkathia@users.noreply.github.com>
2024-05-25 20:16:21 +0000

    criação de exemplo de herança e encapsulamento

---
+++ heranca.py
@@ -0,0 +1,26 @@
+#classe base
+
+class Pessoa:
+    def __init__(self, nome, cpf):
+        self.nome = nome
+        self.__cpf = cpf
+
+    def cumprimentar(self):
+        print(f'Olá ! Tudo bem? Me chamo {self.nome}')
+
+    def setCpf(self, novoCpf):
+        self.__cpf = novoCpf
+
+    def getCpf(self):
+        return self.__cpf 
+
+
+# criação classe mãe que herda atributos e métodos da classe Pessoa
+
+class Mae(Pessoa):
+    def __init__(self, nome, cpf, corPele):
+        super().__init__(nome, cpf)
+        self.corPele = corPele
+
+    def dancar(self):
+        print(f'{self.nome} está dançando')
